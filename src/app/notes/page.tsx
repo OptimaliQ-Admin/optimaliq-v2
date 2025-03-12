9b1249c8c007cc73aa6a3bc1db649c89e0b0004c
KatieLuck <optimaliq.ai@gmail.com>
2025-03-12 15:27:21 -0400

    Update page.tsx

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,3 @@
-import { createClient } from '@/utils/supabase/server';
-
 export default async function Notes() {
   const supabase = await createClient();
   const { data: notes } = await supabase.from("notes").select();
