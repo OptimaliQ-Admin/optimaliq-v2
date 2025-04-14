14967217a350e7a75f97627bcb2fb5e991610f11
KatieLuck <optimaliq.ai@gmail.com>
2025-04-14 12:44:54 -0400

    fixing group render

--- src/app/tier2/assessment/BPM/StepGroupRenderer.tsx
+++ src/app/tier2/assessment/BPM/StepGroupRenderer.tsx
@@ -24,29 +24,29 @@ import Score2_5_Step02 from "./groups/score_2_5/Group02";
 import Score2_5_Step03 from "./groups/score_2_5/Group03";
 
 // Score 3.0 Group
-import Score1_Step01 from "./groups/score_1/Group01";
-import Score1_Step02 from "./groups/score_1/Group02";
-import Score1_Step03 from "./groups/score_1/Group03";
+import Score3_Step01 from "./groups/score_3/Group01";
+import Score3_Step02 from "./groups/score_3/Group02";
+import Score3_Step03 from "./groups/score_3/Group03";
 
 // Score 3.5 Group
-import Score1_5_Step01 from "./groups/score_1_5/Group01";
-import Score1_5_Step02 from "./groups/score_1_5/Group02";
-import Score1_5_Step03 from "./groups/score_1_5/Group03";
+import Score3_5_Step01 from "./groups/score_3_5/Group01";
+import Score3_5_Step02 from "./groups/score_3_5/Group02";
+import Score3_5_Step03 from "./groups/score_3_5/Group03";
 
 // Score 4.0 Group
-import Score1_Step01 from "./groups/score_1/Group01";
-import Score1_Step02 from "./groups/score_1/Group02";
-import Score1_Step03 from "./groups/score_1/Group03";
+import Score4_Step01 from "./groups/score_4/Group01";
+import Score4_Step02 from "./groups/score_4/Group02";
+import Score4_Step03 from "./groups/score_4/Group03";
 
 // Score 4.5 Group
-import Score1_5_Step01 from "./groups/score_1_5/Group01";
-import Score1_5_Step02 from "./groups/score_1_5/Group02";
-import Score1_5_Step03 from "./groups/score_1_5/Group03";
+import Score4_5_Step01 from "./groups/score_4_5/Group01";
+import Score4_5_Step02 from "./groups/score_4_5/Group02";
+import Score4_5_Step03 from "./groups/score_4_5/Group03";
 
 // Score 5.0 Group
-import Score1_Step01 from "./groups/score_1/Group01";
-import Score1_Step02 from "./groups/score_1/Group02";
-import Score1_Step03 from "./groups/score_1/Group03";
+import Score5_Step01 from "./groups/score_5/Group01";
+import Score5_Step02 from "./groups/score_5/Group02";
+import Score5_Step03 from "./groups/score_5/Group03";
 
 type Props = {
   step: number;
@@ -60,8 +60,14 @@ export default function StepGroupRenderer({ step, score, answers, onAnswer }: Pr
 
   const stepGroups: Record<string, React.ComponentType<any>[]> = {
     Score_1: [Score1_Step01, Score1_Step02, Score1_Step03],
-    Score_2_5: [Score2_5_Step01, Score2_5_Step02, Score2_5_Step03],
-    Score_4: [Score4_Step01, Score4_Step02, Score4_Step03],
+  Score_1_5: [Score1_5_Step01, Score1_5_Step02, Score1_5_Step03],
+  Score_2: [Score2_Step01, Score2_Step02, Score2_Step03],
+  Score_2_5: [Score2_5_Step01, Score2_5_Step02, Score2_5_Step03],
+  Score_3: [Score3_Step01, Score3_Step02, Score3_Step03],
+  Score_3_5: [Score3_5_Step01, Score3_5_Step02, Score3_5_Step03],
+  Score_4: [Score4_Step01, Score4_Step02, Score4_Step03],
+  Score_4_5: [Score4_5_Step01, Score4_5_Step02, Score4_5_Step03],
+  Score_5: [Score5_Step01, Score5_Step02, Score5_Step03],
   };
 
   const group = stepGroups[normalizedScore];
@@ -74,6 +80,12 @@ export default function StepGroupRenderer({ step, score, answers, onAnswer }: Pr
 
 function normalizeScore(score: number): string {
   if (score <= 1.5) return "Score_1";
+  if (score <= 2) return "Score_1_5";
+  if (score <= 2.5) return "Score_2";
   if (score <= 3) return "Score_2_5";
-  return "Score_4";
+  if (score <= 3.5) return "Score_3";
+  if (score <= 4) return "Score_3_5";
+  if (score <= 4.5) return "Score_4";
+  if (score <= 5) return "Score_4_5";
+  return "Score_5";
 }
