06150acac76f1e84b669d1e0de5056c8bcf15d40
KatieLuck <optimaliq.ai@gmail.com>
2025-04-16 11:00:19 -0400

    sytax issue fixed
    error is happening because you’re trying to assign a React component (like Score1_Step01) to a place where a validator function is expected — specifically in your validatorSets object
    
    Co-Authored-By: KatieLuck <168120251+KatieLuck@users.noreply.github.com>

--- src/app/tier2/assessment/BPM/StepGroupRenderer.tsx
+++ src/app/tier2/assessment/BPM/StepGroupRenderer.tsx
@@ -6,76 +6,76 @@ import React from "react";
 import Score1_Step01 from "./groups/score_1/Group01";
 import Score1_Step02 from "./groups/score_1/Group02";
 import Score1_Step03 from "./groups/score_1/Group03";
-import isScore_1Group1Complete from "./groups/score_1/Group01";
-import isScore_1Group2Complete from "./groups/score_1/Group02";
-import isScore_1Group3Complete from "./groups/score_1/Group03";
+import {isScore_1Group1Complete} from "./groups/score_1/Group01";
+import {isScore_1Group2Complete} from "./groups/score_1/Group02";
+import {isScore_1Group3Complete} from "./groups/score_1/Group03";
 
 // Score 1.5 Group
 import Score1_5_Step01 from "./groups/score_1_5/Group01";
 import Score1_5_Step02 from "./groups/score_1_5/Group02";
 import Score1_5_Step03 from "./groups/score_1_5/Group03";
-import isScore_1_5Group1Complete from "./groups/score_1_5/Group01";
-import isScore_1_5Group2Complete from "./groups/score_1_5/Group02";
-import isScore_1_5Group3Complete from "./groups/score_1_5/Group03";
+import {isScore_1_5Group1Complete} from "./groups/score_1_5/Group01";
+import {isScore_1_5Group2Complete} from "./groups/score_1_5/Group02";
+import {isScore_1_5Group3Complete} from "./groups/score_1_5/Group03";
 
 // Score 2.0 Group
 import Score2_Step01 from "./groups/score_2/Group01";
 import Score2_Step02 from "./groups/score_2/Group02";
 import Score2_Step03 from "./groups/score_2/Group03";
-import isScore_2Group1Complete from "./groups/score_2/Group01";
-import isScore_2Group2Complete from "./groups/score_2/Group02";
-import isScore_2Group3Complete from "./groups/score_2/Group03";
+import {isScore_2Group1Complete} from "./groups/score_2/Group01";
+import {isScore_2Group2Complete} from "./groups/score_2/Group02";
+import {isScore_2Group3Complete} from "./groups/score_2/Group03";
 
 
 // Score 2.5 Group
 import Score2_5_Step01 from "./groups/score_2_5/Group01";
 import Score2_5_Step02 from "./groups/score_2_5/Group02";
 import Score2_5_Step03 from "./groups/score_2_5/Group03";
-import isScore_2_5Group1Complete from "./groups/score_2_5/Group01";
-import isScore_2_5Group2Complete from "./groups/score_2_5/Group02";
-import isScore_2_5Group3Complete from "./groups/score_2_5/Group03";
+import {isScore_2_5Group1Complete} from "./groups/score_2_5/Group01";
+import {isScore_2_5Group2Complete} from "./groups/score_2_5/Group02";
+import {isScore_2_5Group3Complete} from "./groups/score_2_5/Group03";
 
 // Score 3.0 Group
 import Score3_Step01 from "./groups/score_3/Group01";
 import Score3_Step02 from "./groups/score_3/Group02";
 import Score3_Step03 from "./groups/score_3/Group03";
-import isScore_3Group1Complete from "./groups/score_3/Group01";
-import isScore_3Group2Complete from "./groups/score_3/Group02";
-import isScore_3Group3Complete from "./groups/score_3/Group03";
+import {isScore_3Group1Complete} from "./groups/score_3/Group01";
+import {isScore_3Group2Complete} from "./groups/score_3/Group02";
+import {isScore_3Group3Complete} from "./groups/score_3/Group03";
 
 // Score 3.5 Group
 import Score3_5_Step01 from "./groups/score_3_5/Group01";
 import Score3_5_Step02 from "./groups/score_3_5/Group02";
 import Score3_5_Step03 from "./groups/score_3_5/Group03";
-import isScore_3_5Group1Complete from "./groups/score_3_5/Group01";
-import isScore_3_5Group2Complete from "./groups/score_3_5/Group02";
-import isScore_3_5Group3Complete from "./groups/score_3_5/Group03";
+import {isScore_3_5Group1Complete} from "./groups/score_3_5/Group01";
+import {isScore_3_5Group2Complete} from "./groups/score_3_5/Group02";
+import {isScore_3_5Group3Complete} from "./groups/score_3_5/Group03";
 
 
 // Score 4.0 Group
 import Score4_Step01 from "./groups/score_4/Group01";
 import Score4_Step02 from "./groups/score_4/Group02";
 import Score4_Step03 from "./groups/score_4/Group03";
-import isScore_4Group1Complete from "./groups/score_4/Group01";
-import isScore_4Group2Complete from "./groups/score_4/Group02";
-import isScore_4Group3Complete from "./groups/score_4/Group03";
+import {isScore_4Group1Complete} from "./groups/score_4/Group01";
+import {isScore_4Group2Complete} from "./groups/score_4/Group02";
+import {isScore_4Group3Complete} from "./groups/score_4/Group03";
 
 // Score 4.5 Group
 import Score4_5_Step01 from "./groups/score_4_5/Group01";
 import Score4_5_Step02 from "./groups/score_4_5/Group02";
 import Score4_5_Step03 from "./groups/score_4_5/Group03";
-import isScore_4_5Group1Complete from "./groups/score_4_5/Group01";
-import isScore_4_5Group2Complete from "./groups/score_4_5/Group02";
-import isScore_4_5Group3Complete from "./groups/score_4_5/Group03";
+import {isScore_4_5Group1Complete} from "./groups/score_4_5/Group01";
+import {isScore_4_5Group2Complete} from "./groups/score_4_5/Group02";
+import {isScore_4_5Group3Complete} from "./groups/score_4_5/Group03";
 
 
 // Score 5.0 Group
 import Score5_Step01 from "./groups/score_5/Group01";
 import Score5_Step02 from "./groups/score_5/Group02";
 import Score5_Step03 from "./groups/score_5/Group03";
-import isScore_5Group1Complete from "./groups/score_5/Group01";
-import isScore_5Group2Complete from "./groups/score_5/Group02";
-import isScore_5Group3Complete from "./groups/score_5/Group03";
+import {isScore_5Group1Complete} from "./groups/score_5/Group01";
+import {isScore_5Group2Complete} from "./groups/score_5/Group02";
+import {isScore_5Group3Complete} from "./groups/score_5/Group03";
 
 
 type Props = {
