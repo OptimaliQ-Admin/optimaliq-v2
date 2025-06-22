cc7cf7ab26d4e5ad498a7a2dc4f99db5e044d5b4
James Luck <jamesluck@Jamess-Air.attlocal.net>
2025-06-22 16:57:16 -0400

    mod 4 CMP Added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ✅ Optional: Customize other settings here
-
-  // ✅ Middleware matcher — tells Next.js where middleware should apply
-  matcher: ["/premium/:path*"],
 };
 
 export default nextConfig;
